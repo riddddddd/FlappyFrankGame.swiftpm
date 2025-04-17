67d8e4a489ad78fb6535e8e2e97774e5cf7365db
Riddick Variano <ridisepic@gmail.com>
2025-04-17 11:00:14 -0700

    Made Frank load with the scene

--- FlappyFrank.swift
+++ FlappyFrank.swift
@@ -10,19 +10,28 @@ import SpriteKit
 class FlappyFrank: SKScene{
     let Frank = SKSpriteNode(imageNamed: "Frank")
 
-        override func didMove(to view: SKView) {
-            backgroundColor = .cyan
-                
-            
-          
-            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
-            Frank.setScale(0.5)
-            addChild(Frank)
-        }
+    let Pipe = SKSpriteNode(imageNamed: "Pipe")
+    
+    
+        
+    
+    
+    /*This keeps crashing it*/
+//        override func didMove(to view: SKView) {
+//            backgroundColor = .cyan
+//                
+//            
+//          
+//            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
+//            Frank.setScale(0.5)
+//            addChild(Frank)
+//        }
     
     override func sceneDidLoad() {
         //sky
         self.backgroundColor = .cyan
+        self.addChild(Frank)
+        Frank.position = CGPoint(x: 500, y: 500)
     }
     
     
