5aeff8856caf3783057a54e63f8d1fc72ca4af91
Riddick Variano <ridisepic@gmail.com>
2025-04-17 11:35:12 -0700

    added child pipe, all the commented stuff is crashing it

--- FlappyFrank.swift
+++ FlappyFrank.swift
@@ -9,54 +9,59 @@ import SpriteKit
 
 class FlappyFrank: SKScene{
     let Frank = SKSpriteNode(imageNamed: "Frank")
-
+    
     let Pipe = SKSpriteNode(imageNamed: "Pipe")
     
-    
-        
-    
-    
-    /*This keeps crashing it*/
-//        override func didMove(to view: SKView) {
-//            backgroundColor = .cyan
-//                
-//            
-//          
-//            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
-//            Frank.setScale(0.5)
-//            addChild(Frank)
-//        }
     
     override func sceneDidLoad() {
         //sky
         self.backgroundColor = .cyan
+        
+        //Frank
         self.addChild(Frank)
         Frank.position = CGPoint(x: 500, y: 500)
-        override func didMove(to view: SKView) {
-            backgroundColor = .cyan
-                
-            
-          
-            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
-            Frank.setScale(0.5)
-            addChild(Frank)
-            
-            
-            Frank.physicsBody = SKPhysicsBody(rectangleOf: Frank.size)
-            Frank.physicsBody? .affectedByGravity = true
-            Frank.physicsBody? .allowsRotation = false
-            
-            
-            
-            physicsWorld.gravity = CGVector(dx: 0, dy: -5)
-            
-            
-        }
-    func flap() {
-        Frank.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        Frank.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
+        
+        //Pipe (work in progress)
+        self.addChild(Pipe)
+        Pipe.position = CGPoint(x: 200, y: 200)
+        
+        
+        
     }
- 
+    
+
+    
+    
+    
+    
+    /*This keeps crashing it*/
+    //        override func didMove(to view: SKView) {
+    //            backgroundColor = .cyan
+    //
+    //
+    //
+    //            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
+    //            Frank.setScale(0.5)
+    //            addChild(Frank)
+    //        }
+    
+    
+    
+    
+//    override func didMove(to view: SKView) {
+//        Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
+//        Frank.setScale(0.5)
+//        addChild(Frank)
+//        Frank.physicsBody = SKPhysicsBody(rectangleOf: Frank.size)
+//        Frank.physicsBody? .affectedByGravity = true
+//        Frank.physicsBody? .allowsRotation = false
+//        physicsWorld.gravity = CGVector(dx: 0, dy: -5)
+//    }
+//    func flap() {
+//        Frank.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+//        Frank.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
+//    }
+//    
     
     
 }
