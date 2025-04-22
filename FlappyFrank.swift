8baeb8af756d43b3ebcdbb4509cd8765eca43597
Riddick Variano <ridisepic@gmail.com>
2025-04-22 10:49:59 -0700

    tried to use sktexture to load the image

--- FlappyFrank.swift
+++ FlappyFrank.swift
@@ -8,7 +8,7 @@ import SwiftUI
 import SpriteKit
 
 class FlappyFrank: SKScene{
-    let Frank = SKSpriteNode(imageNamed: "Frank")
+    let Frank = SKSpriteNode(texture: SKTexture(imageNamed: "Frank"))
     
     let Pipe = SKSpriteNode(imageNamed: "Pipe")
     
@@ -19,49 +19,10 @@ class FlappyFrank: SKScene{
         
         //Frank
         self.addChild(Frank)
-        Frank.position = CGPoint(x: 500, y: 500)
+        Frank.position = CGPoint(x: 300 , y: 250)
         
-        //Pipe (work in progress)
+        //Pipe
         self.addChild(Pipe)
         Pipe.position = CGPoint(x: 200, y: 200)
-        
-        
-        
     }
-    
-
-    
-    
-    
-    
-    /*This keeps crashing it*/
-    //        override func didMove(to view: SKView) {
-    //            backgroundColor = .cyan
-    //
-    //
-    //
-    //            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
-    //            Frank.setScale(0.5)
-    //            addChild(Frank)
-    //        }
-    
-    
-    
-    
-//    override func didMove(to view: SKView) {
-//        Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
-//        Frank.setScale(0.5)
-//        addChild(Frank)
-//        Frank.physicsBody = SKPhysicsBody(rectangleOf: Frank.size)
-//        Frank.physicsBody? .affectedByGravity = true
-//        Frank.physicsBody? .allowsRotation = false
-//        physicsWorld.gravity = CGVector(dx: 0, dy: -5)
-//    }
-//    func flap() {
-//        Frank.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-//        Frank.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
-//    }
-//    
-    
-    
 }
