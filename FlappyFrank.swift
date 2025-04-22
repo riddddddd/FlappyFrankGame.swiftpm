967bf481173b427791b57a012a2037f1fbd4f025
Alexander Kuklinski <akuklinski7210@stu.d214.org>
2025-04-22 12:47:16 -0500

    I cleaned up unnecesary code and added a function where if you do a tap gesture the bird will go up

--- FlappyFrank.swift
+++ FlappyFrank.swift
@@ -9,54 +9,35 @@ import SpriteKit
 
 class FlappyFrank: SKScene{
     let Frank = SKSpriteNode(imageNamed: "Frank")
-
+    
     let Pipe = SKSpriteNode(imageNamed: "Pipe")
     
     
+    
+    
+    
+    
+    override func didMove(to view: SKView) {
         
-    
-    
-    /*This keeps crashing it*/
-//        override func didMove(to view: SKView) {
-//            backgroundColor = .cyan
-//                
-//            
-//          
-//            Frank.position = CGPoint(x: size.width / 2, y: size.height / 2)
-//            Frank.setScale(0.5)
-//            addChild(Frank)
-//        }
-    
-    override func sceneDidLoad() {
-        //sky
-        self.backgroundColor = .cyan
-        self.addChild(Frank)
-        Frank.position = CGPoint(x: 500, y: 500)
-        override func didMove(to view: SKView) {
-            backgroundColor = .cyan
-                
-            
-          
-            Frank.position = CGPoint(x: size.width / 2 - 100, y: size.height / 2)
-            Frank.setScale(0.5)
-            addChild(Frank)
-            
-            
-            Frank.physicsBody = SKPhysicsBody(rectangleOf: Frank.size)
-            Frank.physicsBody? .affectedByGravity = true
-            Frank.physicsBody? .allowsRotation = false
-            
-            
-            
-            physicsWorld.gravity = CGVector(dx: 0, dy: -5)
-            
-            
-        }
+        backgroundColor = .cyan
+        Frank.position = CGPoint(x: size.width / 2 - 100, y: size.height / 2)
+        Frank.setScale(0.5)
+        Frank.physicsBody = SKPhysicsBody(rectangleOf: Frank.size)
+        Frank.physicsBody?.affectedByGravity = true
+        Frank.physicsBody?.allowsRotation = false
+        addChild(Frank)
+        physicsWorld.gravity = CGVector(dx: 0, dy: -10)
+       
+    }
     func flap() {
         Frank.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        Frank.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 150))
+        Frank.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 130))
     }
- 
     
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        flap()
+    }
+
 }
+
