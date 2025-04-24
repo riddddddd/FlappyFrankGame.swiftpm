8426c1eeca23e0a9871ba4f3c48378df4e25c494
Shawn Simon <Ssimon5338@stu.d214.org>
2025-04-24 12:58:14 -0500

    reset button

--- ContentView.swift
+++ ContentView.swift
@@ -3,25 +3,39 @@ import SpriteKit
 
 
 struct ContentView: View {
+    @State private var score = 0
+    @State private var highScore = UserDefaults.standard.integer(forKey: "HighScore")
     var body: some View {
+        
         ZStack{
             Image("Background")
                 .resizable()
                 .ignoresSafeArea()
         }
+        
+        
+        
         VStack{
             SpriteView(scene: scene)
                 .ignoresSafeArea()
         }
         var scene: SKScene {
             let scene = FlappyFrank()
-                scene.size = CGSize(width: 200, height: 400)
-                scene.scaleMode = .resizeFill
-                return scene
-            }
-
-            
-    }
-
+            scene.size = CGSize(width: 200, height: 400)
+            scene.scaleMode = .resizeFill
+            return scene
         }
-      
+        
+        Button(action: {
+            score = 0
+        }) {
+            Text("Reset")
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+            }
+    
+}
+
