29eb1d5579f892d358e6f7d9e6be428924742403
Will Baik <wbaik7022@stu.d214.org>
2025-04-24 10:30:48 -0700

    Background

--- ContentView.swift
+++ ContentView.swift
@@ -4,15 +4,24 @@ import SpriteKit
 
 struct ContentView: View {
     var body: some View {
-        SpriteView(scene: scene)
-            .ignoresSafeArea()
-    }
-    var scene: SKScene {
-        let scene = FlappyFrank()
-            scene.size = CGSize(width: 200, height: 400)
-            scene.scaleMode = .resizeFill
-            return scene
+        ZStack{
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
         }
+        VStack{
+            SpriteView(scene: scene)
+                .ignoresSafeArea()
+        }
+        var scene: SKScene {
+            let scene = FlappyFrank()
+                scene.size = CGSize(width: 200, height: 400)
+                scene.scaleMode = .resizeFill
+                return scene
+            }
 
-        
-}
+            
+    }
+
+        }
+      
